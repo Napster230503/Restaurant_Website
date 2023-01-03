4a6a3b2cfcacff11511d6dbcf7c53b5ee9278e0b
Saiful <kriegswar27@gmail.com>
2023-01-03 08:07:58 +0700

    nih mi, yg menu ama emp dh w add

---
+++ Admin/pages/partIndex/sideNavbar.php
@@ -0,0 +1,58 @@
+<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+          <div class="sb-sidenav-menu">
+            <div class="nav">
+              <div class="sb-sidenav-menu-heading">Table's</div>
+              <a class="nav-link" href="index.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                Employee
+              </a>
+              <a class="nav-link" href="item.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                Item
+              </a>
+              <a class="nav-link" href="customer.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                Customer
+              </a>
+              <a class="nav-link" href="menu.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                Menu
+              </a>
+              <div class="sb-sidenav-menu-heading">Interface</div>
+              <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                Layouts
+                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+              </a>
+              <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <nav class="sb-sidenav-menu-nested nav">
+                  <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                </nav>
+              </div>
+              <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                Pages
+                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+              </a>
+              <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                  <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                    Authentication
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                  </a>
+                  <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                    <nav class="sb-sidenav-menu-nested nav">
+                      <a class="nav-link" href="login.html">Login</a>
+                      <a class="nav-link" href="register.html">Register</a>
+                      <a class="nav-link" href="password.html">Forgot Password</a>
+                    </nav>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            Admin
+          </div>
+        </nav>
